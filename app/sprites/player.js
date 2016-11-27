3e4403df8de27d88645b3e9cddbe4c82b85b622d
Zosia Wiśniowolska <sobocinska.zosia@gmail.com>
2016-11-27 21:33:20 +0100

    Create sprite for player character and let him turn left and right (#7)
    * Update player_sheet image!
    
    * Update player_sheet image again by adding details!
    
    * Change red light color!
    
    * Adapt player height
    
    * Update player_sheet! Left and right!
    
    * Make Rysiek turn left and right
    
    * Add serve-static to dependencies (this should be done some time ago already)

--- app/sprites/player.js
+++ app/sprites/player.js
@@ -7,18 +7,36 @@ define([
 ) {
   var ASSET_FILENAME = 'player_sheet.png';
   var ASSET_LABEL = 'player';
+
+  Q.animations('player', {
+    run_right: {
+      frames: [1]
+    },
+    run_left: {
+      frames: [2]
+    }
+  });
+
   Q.Sprite.extend("Player", {
     init: function(p) {
       this._super(p, {
+        sprite: 'player',
         sheet: 'player'
       });
-      this.add('2d, platformerControls');
+      this.add('2d, platformerControls, animation');
+    },
+    step: function() {
+      if(this.p.vx > 0) {
+        this.play('run_right');
+      } else if(this.p.vx < 0) {
+        this.play('run_left');
+      }
     }
   });
 
   var createSheet = function createSheet() {
     Q.sheet(ASSET_LABEL, ASSET_FILENAME, {
-      tilew: 30, tileh: 24
+      tilew: 24, tileh: 49
     });
   };
 
