0af800bb89f9a3f609d074fbe8fdd33286522ee1
Zosia Sobocinska <Zosia Sobocinska>
2017-01-08 19:15:05 +0100

    Create Q helper to load asset and create sheet in a single step

--- app/q.js
+++ app/q.js
@@ -1,10 +1,27 @@
-define(['quintus'], function(quintus) {
-  var Q = quintus({ development: true })
-    .include("Sprites, Scenes, Input, 2D, UI, Anim")
-    .setup({
-      maximize: true
-    })
-    .controls();
+define([
+    'quintus',
+    'promise'
+  ], function(
+    quintus,
+    Promise
+  ) {
+    var Q = quintus({ development: true })
+      .include("Sprites, Scenes, Input, 2D, UI, Anim")
+      .setup({
+        maximize: true
+      })
+      .controls();
 
-  return Q;
-});
+    Q.sheetPromise = function(sheetName, fileName) {
+      var deferred = Promise.defer();
+      var args = arguments;
+      Q.load(fileName, function() {
+        Q.sheet.apply(Q, args);
+        deferred.resolve();
+      });
+
+      return deferred.promise;
+    };
+
+    return Q;
+  });
